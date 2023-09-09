7d90dc11a030e72a8baee8d4593fc144338d41da
manu <manu.bsdt@gmail.com>
2023-09-09 16:30:25 +0530

    chages in price detals

--- lib/screens/pricelist/priceview.dart
+++ lib/screens/pricelist/priceview.dart
@@ -1,16 +1,90 @@
 import 'package:flutter/material.dart';
 
 class Price extends StatelessWidget {
-  Price({Key? key}) : super(key: key);
+  const Price({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text(
-        "Price screen",
-        style: TextStyle(fontSize: 15),
+      body: CustomScrollView(
+        slivers: [
+          SliverGrid.count(
+              crossAxisCount: 4,
+              childAspectRatio:0.4, // Adjust the aspect ratio as needed
+              children: List.generate(
+                6,
+                (index) => GestureDetector(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: const DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(
+                                    "https://5.imimg.com/data5/TK/SW/ZV/SELLER-88265349/waste-old-newspaper-scrap-500x500.jpg",
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.transparent,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                // Use EdgeInsets.all for consistent padding
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "News Paper",
+                                      style: TextStyle(
+                                        color: Colors.grey[600],
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    const Row(
+                                      children: [
+                                        Text(
+                                          '\u20B925',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ))
+        ],
       ),
-    ));
+    );
   }
 }
